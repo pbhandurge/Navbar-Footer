856545285d1e233b17498c556c4f974f58178e5c
Pradnya Pratap Bhandurage <120319947+pbhandurge@users.noreply.github.com>
2024-05-15 16:57:02 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import React from "react";
+import Navbar  from "./components/Navbar";
+
+
+export default function Home() {
+  return (
+    <>
+    <div>Hello world</div>
+  
+    </>
+  )
+  
+}
